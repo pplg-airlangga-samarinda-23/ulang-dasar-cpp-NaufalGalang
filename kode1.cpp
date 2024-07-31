7930ce720c16a437468922d9b99a3a4781d10e27
NaufalGalang <galangpelo123@gmail.com>
2024-07-31 11:26:38 +0800

    selesai tugas

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void cetakdata(string nama, string nisn, string jurusan, string tinggibadan) {
+    cout << "Nama: " << nama << endl;
+    cout << "NISN: " << nisn << endl;
+    cout << "Jurusan: " << jurusan << endl;
+    cout << "Tinggi Badan: " << tinggibadan << endl;
+}
+
 int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
-    return 0;
+    string nama = "Naufal Galang Saputra";
+    string nisn = "0086572306";
+    string jurusan = "PPLG";
+    string tinggibadan = "172";
+    
+    cetakdata(nama, nisn, jurusan, tinggibadan);
+    
+
+    return 0;
+
 }
